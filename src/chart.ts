24d4b464016ce7cb04470664328ded28810eecbb
Brian McClune <brian.mcclune@unnpp.gov>
2022-08-15 16:46:26 -0400

    Update scaffold src/ with d3-me content

---
+++ src/chart.ts
@@ -0,0 +1,136 @@
+import * as d3 from 'd3'
+
+interface Margin {
+  top: number
+  right: number
+  bottom: number
+  left: number
+}
+
+export interface Config {
+  margin: Margin
+  width: number
+  height: number
+  duration: number
+  domain: [number, number]
+  xlabel: string
+  ylabel: string
+}
+
+type Datum = [Date, number]
+
+type Selection = d3.Selection<d3.BaseType, Datum[], HTMLElement, any>
+type ValueFn = d3.ValueFn<d3.BaseType, unknown, d3.KeyType>
+
+export function streamingChart(config?: Config) {
+  let margin = config?.margin || {top: 20, right: 20, bottom: 60, left: 60}
+  let width = config?.width || 600
+  let height = config?.height || 400
+  let duration = config?.duration || 200
+  let yDomain = config?.domain || [-1, 1]
+  let xlabel = config?.xlabel || "x"
+  let ylabel = config?.ylabel || "y"
+
+  let x = d3.scaleUtc()
+  let y = d3.scaleLinear()
+
+  function chart(selection: Selection) {
+    selection.each(function(data: Datum[]) {
+
+      // Update the scales.
+      let xDomain = d3.extent(
+        data,
+        (d: Datum) => d[0],
+      ).map(v => v!)
+      x
+        .domain(xDomain)
+        .range([0, width - margin.left - margin.right])
+      y
+        .domain(yDomain)
+        .range([height - margin.top - margin.bottom, 0])
+
+      // Create the skeletal chart if necessary.
+      let gEnter = d3.select(this).selectAll("svg")
+          .data([data])
+          .enter()
+        .append("svg")
+        .append("g")
+      gEnter
+        .append("g")
+          .attr("class", "x axis")
+      gEnter
+        .append("g")
+        .append("text")
+          .attr("x", (width - margin.left - margin.right) / 2)
+          .attr("y", height - margin.bottom / 2)
+          .attr("text-anchor", "start")
+          .attr("fill", "black")
+          .text(xlabel)
+      gEnter
+        .append("g")
+          .attr("class", "y axis")
+      gEnter
+        .append("g")
+        .append("text")
+          .attr("x", -(height - margin.top - margin.bottom) / 2)
+          .attr("y", -margin.left / 2)
+          .attr("text-anchor", "start")
+          .attr("fill", "black")
+          .attr("transform", "rotate(-90)")
+          .text(ylabel)
+      gEnter
+        .append("defs").append("clipPath")
+          .attr("id", "clip")
+        .append("rect")
+          .attr("width", width - margin.left - margin.right)
+          .attr("height", height - margin.top - margin.bottom)
+      gEnter
+        .append("g")
+          .attr("clip-path", "url(#clip)")
+          .attr("class", "data")
+          .attr("fill", "none")
+          .attr("stroke-linecap", "round")
+
+      // Update the out dimensions.
+      let svg = d3.select(this).select("svg")
+          .attr("viewBox", `0 0 ${width} ${height}`)
+
+      // Update the inner dimensions.
+      let g = svg.select("g")
+          .attr("transform", `translate(${margin.left},${margin.top})`)
+
+      // Update the line path.
+      svg.select("g.data").selectAll("path")
+          .data(data, ((d: [number, number]) => d[0]) as ValueFn)
+          .join(
+            enter => enter.append("path")
+              .attr("d", d => (
+                `M${x(new Date(d[0].getTime() + duration))},${y(d[1])}h0`
+              ))
+              .attr("stroke", "blue")
+              .attr("stroke-width", 5)
+              .call(enter => enter.transition()
+                .duration(duration)
+                .ease(d3.easeLinear)
+                .attr("d", d => `M${x(d[0])},${y(d[1])}h0`)
+              ),
+            update => update
+              .call(update => update.transition()
+                .duration(duration)
+                .ease(d3.easeLinear)
+                .attr("d", d => `M${x(d[0])},${y(d[1])}h0`)
+              ),
+          )
+
+      // Update the axes.
+      g.select<SVGGElement>(".x.axis")
+          .attr("transform", `translate(0,${y.range()[0]})`)
+          .transition().duration(duration).ease(d3.easeLinear)
+          .call(d3.axisBottom(x).ticks(5))
+      g.select<SVGGElement>(".y.axis")
+          .call(d3.axisLeft(y).ticks(5))
+    })
+  }
+
+  return chart
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import { Config, streamingChart } from './chart'
+
+import * as d3 from 'd3'
+
+interface Response {
+  x: number,
+  y: number,
+}
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -12,12 +19,51 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button id="start" type="button">Start</button>
+      <button id="stop" type="button">Stop</button>
+      <button id="reset" type="button">Reset</button>
     </div>
-    <p class="read-the-docs">
+    <p id="latest" class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let running = false;
+let plot_data: Array<[Date, number]> = []
+
+document.getElementById("start")!.addEventListener("click", () => {
+  running = true
+})
+
+document.getElementById("stop")!.addEventListener("click", () => {
+  running = false
+})
+
+document.getElementById("reset")!.addEventListener("click", () => {
+  plot_data = []
+})
+
+let duration = 200
+let chart = streamingChart(({
+  duration,
+  xlabel: "t",
+  ylabel: "sin(t)",
+} as Config))
+
+while (true) {
+  if (running) {
+    let response = await fetch('http://localhost:8000/')
+    let data: Response = await response.json()
+    let data_point: [Date, number] = [new Date(data.x * 1000), data.y]
+    plot_data.push(data_point)
+    let s = data_point[1] < 0 ? '' : '+'
+    document.getElementById("latest")!.textContent =
+      `${data_point[0].toLocaleTimeString()}: ${s}${data_point[1].toFixed(2)}`
+    if (plot_data.length > 50) plot_data.shift()
+    d3.select("#app")
+      .data([plot_data])
+      .call(chart)
+  }
+  await new Promise(r => setTimeout(r, duration))
+}
