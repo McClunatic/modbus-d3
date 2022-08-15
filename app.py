0de568a6a77388164845d766b36b630c2958b010
Brian McClune <brian.mcclune@unnpp.gov>
2022-08-15 16:58:00 -0400

    Add CORS policy to app

--- app.py
+++ app.py
@@ -9,11 +9,22 @@ from typing import Tuple
 import numpy as np
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pymodbus.client.asynchronous.tcp import AsyncModbusTCPClient
 from pymodbus.client.asynchronous import schedulers
 
 #: FastAPI application
 app = FastAPI()
+
+origins = ['http://127.0.0.1:5173']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 #: Application log
 app_log = logging.getLogger('modbus.d3.app')
